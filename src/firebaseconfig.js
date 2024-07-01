e10528671e86100dd705f17df07091867b92e778
jaykishankava <kava.jaykishan9125@gmail.com>
2024-07-01 22:27:27 +0530

    Crud-firebase

---
+++ src/firebaseconfig.js
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBqbb4Jlcb5lPGGOMvi-OUYmKOXgt48WmY",
+  authDomain: "fir-firestore-68e71.firebaseapp.com",
+  projectId: "fir-firestore-68e71",
+  storageBucket: "fir-firestore-68e71.appspot.com",
+  messagingSenderId: "890436074595",
+  appId: "1:890436074595:web:630f1e48e3395cbc024c56",
+  measurementId: "G-M4B33Q2FSZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
